0d2320a80ea80808de806642175abba33767aeca
Matias P. Brutti <matiasbrutti@gmail.com>
2014-04-06 18:11:45 -0700

    Small fixes & updates
    - Updated Bootstrap to 3.1.1
    - Updated Jquery 1.11.1
    - Updated Jquery.json2html

--- website/js/posts.js
+++ website/js/posts.js
@@ -22,62 +22,99 @@ var post_data_url = "sample.json";
 
 
 if (blog_title != "") {
-  $("#blogtitle").text(blog_title);
+    $("#blogtitle").text(blog_title);
 }
- 
+
 // Fetch and process post_data
 var jqxhr = $.getJSON(post_data_url);
-jqxhr.complete(function() { 
-  var simpleblog = jQuery.parseJSON(jqxhr.responseText);
-  var tags_transform = 
-  { tag:'div', id:'posttag', 
-    class: 'label label-info', 
-    html : '${tag}'
-  };
-  
-  var id = 0;
-  var transform = 
-  [{tag:'div', id: function(){id=id+1; return("post_"+id);}, class:'row',children:[
-    {tag:'div',class:'span2',children:[
-      {tag: 'img', id: 'postimage', class: 'img-circle', src: '${authorimage}'},
-    ]},
-    {tag:'div',class:'span10',children:[
-      {tag:'h4', id: 'posttitle', html:'${title}'},
-      {tag:'h6', id: 'postauthor', html:'${author}  ', children: [{tag: 'small', html: ' -  ${timestamp}'}]}    
-      ]},
-    {tag:'div', class:'span12',children:[
-      {tag:'hr', class:'soften'}
-    ]},
-    {tag:'div', id: 'posttext', class:'span12',children:[
-      {tag:'p',  html: function(){
-        return(markdown.toHTML(this.content));
-      }},
-      {tag:'div', children: function(){
-        return(json2html.transform(this.tags,tags_transform));
-      }}
-    ]}
-  ]}, {tag:'br'}];
+jqxhr.complete(function() {
+    var simpleblog = jQuery.parseJSON(jqxhr.responseText);
+    var tags_transform = {
+        tag: 'div',
+        id: 'posttag',
+        class: 'label label-info',
+        html: '${tag}'
+    };
 
-  $('#PostsContainer').json2html(simpleblog.reverse(),transform);
-  $('#PostsContainer').paginate({itemsPerPage: post_per_page * 3});
- });
+    var id = 0;
+    var transform =
+        [{
+        tag: 'div',
+        id: function() {
+            id = id + 1;
+            return ("post_" + id);
+        },
+        class: 'row',
+        children: [{
+            tag: 'div',
+            class: 'span2',
+            children: [{
+                tag: 'img',
+                id: 'postimage',
+                class: 'img-circle',
+                src: '${authorimage}'
+            }, ]
+        }, {
+            tag: 'div',
+            class: 'span10',
+            children: [{
+                tag: 'h4',
+                id: 'posttitle',
+                html: '${title}'
+            }, {
+                tag: 'h6',
+                id: 'postauthor',
+                html: '${author}  ',
+                children: [{
+                    tag: 'small',
+                    html: ' -  ${timestamp}'
+                }]
+            }]
+        }, {
+            tag: 'div',
+            class: 'span12',
+            children: [{
+                tag: 'hr',
+                class: 'soften'
+            }]
+        }, {
+            tag: 'div',
+            id: 'posttext',
+            class: 'span12',
+            children: [{
+                tag: 'p',
+                html: function() {
+                    return (markdown.toHTML(this.content));
+                }
+            }, {
+                tag: 'div',
+                children: function() {
+                    return (json2html.transform(this.tags, tags_transform));
+                }
+            }]
+        }]
+    }, {
+        tag: 'br'
+    }];
 
-
-$(document).ready(function () {
- 
-    $(document).keydown(function(e) {
-        if (e.which == 37) {  // Left arrow key code
-            $('#PostsContainer-previous').click();
-        }
-        else if (e.which == 39) {  // Right arrow key code
-            $('#PostsContainer-next').click();
-        }
-        else if (e.which == 75) {
-          // TODO: Implement j/k navigation from post to post.
-        }
-        else if (e.which == 74) {
-          // TODO: Implement j/k navigation from post to post. 
-        };
+    $('#PostsContainer').json2html(simpleblog.reverse(), transform);
+    $('#PostsContainer').paginate({
+        itemsPerPage: post_per_page * 3
     });
 });
 
+
+$(document).ready(function() {
+
+    $(document).keydown(function(e) {
+        if (e.which == 37) { // Left arrow key code
+            $('#PostsContainer-previous').click();
+        } else if (e.which == 39) { // Right arrow key code
+            $('#PostsContainer-next').click();
+        } else if (e.which == 75) {
+            // TODO: Implement j/k navigation from post to post.
+        } else if (e.which == 74) {
+            // TODO: Implement j/k navigation from post to post. 
+        };
+    });
+});
